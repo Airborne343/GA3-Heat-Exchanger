fb1a0e17af29a380a044118222c5c14022324efe
Iain Lam <yhl58@cam.ac.uk>
2025-05-16 16:15:56 +0100

    Adding hydr

--- hydraulicloss.py
+++ hydraulicloss.py
@@ -1,7 +1,14 @@
 import numpy as np
+from Datatables import Friction
 from HXobj import HeatExchanger 
 
 x = HeatExchanger(tube_count = 13, baffle_count = 3, type = "tri")
 
-Qguess = 100 #Cm3/s
-Pipevelocity = Qguess/x.
+Qguess = 0.3/1000
+Pipevelocity = Qguess/x.pipearea
+Pipedynpressure = 1/2 * 1000 * Pipevelocity ** 2
+Re_pipe = 
+
+P_pipe = Pipedynpressure * x.length / x.tube_ID * Friction(Re_pipe)
+
+print(P_pipe)
