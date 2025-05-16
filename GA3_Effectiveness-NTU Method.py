f93de8c88781421c7a8199b0ffc832409798825a
Airborne343 <rb2026@cam.ac.uk>
2025-05-16 15:29:39 +0100

    ENTU + LMTD

--- GA3_Effectiveness-NTU Method.py
+++ GA3_Effectiveness-NTU Method.py
@@ -19,6 +19,7 @@ L = 0.35
 d_inner = 0.006
 N_tubes = 13
 A_ht = N_tubes * np.pi * L * d_inner
+F = 1 
 
 #ENTU Method
 def effective_NTU(H, N_tubes, m_hot, Thot_in, m_cold, Tcold_in, cp_w, N_shell):
@@ -43,7 +44,7 @@ def effective_NTU(H, N_tubes, m_hot, Thot_in, m_cold, Tcold_in, cp_w, N_shell):
     q_abs = eff * q_max 
 
     Thot_out = Thot_in - q_abs/C_hot
-    Tcold_out = Tcold_in - q_abs/C_cold
+    Tcold_out = Tcold_in + q_abs/C_cold
 
     return [NTU, eff, Thot_out, Tcold_out, q_abs]
 
@@ -56,3 +57,38 @@ print(f"ENTU Cold Side Outlet Temperature: {result[3]:.2f} C")
 print(f"ENTU Heat Transfer: {result[4]:.2f} W")
 
 #LMTD Method
+n_iter = 100
+convergence_thresh = 1e-4
+
+C_hot = m_hot * cp_w
+C_cold = m_cold * cp_w
+
+Thot_out_init = 59.99
+
+for i in range(n_iter):
+    Q_val = C_hot * (Thot_in - Thot_out_init)
+    Tcold_out = Tcold_in + Q_val/C_cold
+
+    Delta_T1 = Thot_in - Tcold_out
+    Delta_T2 = Thot_out_init - Tcold_in
+
+    if Delta_T1 <=0 or Delta_T2 <=0:
+        raise ValueError("Unrealistic Guess for Delta T")
+    
+    LMTD = (Delta_T1 - Delta_T2)/np.log(Delta_T1/Delta_T2)
+    Q_LMTD = H * A_ht * F * LMTD
+    Thot_out_LMTD = Thot_in - Q_LMTD/C_hot
+    
+    if abs(Thot_out_LMTD - Thot_out_init) < convergence_thresh:
+        break
+
+    Thot_out_init = 0.1 * Thot_out_LMTD + 0.9 * Thot_out_init
+
+Q_fn = (Thot_in - Thot_out_LMTD) * C_hot
+Tcold_out_fn = Tcold_in + Q_fn/C_cold
+
+print(f"Convergence in {i+1} iterations:")
+print(f"Hot Side Outlet Temperature: {Thot_out_LMTD:.2f} C")
+print(f"Cold Side Outlet Temperature: {Tcold_out_fn:.2f} C")
+print(f"LMTD {LMTD:.2f} C")
+print(f"Heat Transfer {Q_fn:.2f} W")
