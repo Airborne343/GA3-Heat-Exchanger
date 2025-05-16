fixed tables to be in SI units

Data tables.py
@@ -2,30 +2,31 @@ import numpy as np
 from scipy.optimize import fsolve
 
 #Tables for compressor chics
-def Coldchic(p = None, mdot = None): #cm3/s to Pa
+def Coldchic(p = None, qdot = None): #cm3/s to Pa
     if p == None: 
         poly = np.poly1d([-2.03241506e-07,  2.18666233e-04, -1.17200431e-01, -4.34105885e+01,
   7.07758580e+04])
-        return poly(mdot)
+        return poly(qdot * 10**6) 
     
-    elif mdot == None:
+    elif qdot == None:
         poly = np.poly1d([-1.12242099e-17,  1.58646220e-12, -1.69351833e-07, -2.28742118e-03,
   7.29640030e+02])
-        return poly(p)
+        return poly(p) / 10**6
     
     else:
         raise("Invalid input, only input one of pressure or mass flow")
           
-def Hotchic(p = None, mdot = None): #cm3/s to Pa
+
+def Hotchic(p = None, qdot = None): #cm3/s to Pa
     if p == None: 
         poly = np.poly1d([1.30073533e-06, -1.00398975e-03,  9.91494147e-02, -8.16106902e+01,
   6.23799629e+04])
-        return poly(mdot)
+        return poly(qdot * 10**6) 
     
-    elif mdot == None:
+    elif qdot == None:
         poly = np.poly1d([-8.70199042e-18, -4.64584399e-13,  6.03677597e-08, -7.97101169e-03,
   5.06006301e+02])
-        return poly(p)
+        return poly(p) / 10**6
     
     else:
         raise("Invalid input, only input one of pressure or mass flow")
@@ -34,6 +35,7 @@ def Hotchic(p = None, mdot = None): #cm3/s to Pa
 def Friction(Re = None, epsilon = 0.002, D = 8 ): #https://www.engineeringtoolbox.com/surface-roughness-ventilation-ducts-d_209.html
     #Using Haarland equation
     return (-1.8 * np.log10((epsilon / D / 3.7)**1.11 + 6.9 / Re))**-2
+
 
 def Kc(sigma, Re):
     #These fitting is determined via Figure 8 in the handout
@@ -67,3 +69,5 @@ def Ke(sigma, Re):
 
     # Return the combined cubic‐in‐sigma polynomial
     return d3*σ**3 + d2*σ**2 + d1*σ + d0
+
+print(Coldchic(qdot= 0.658/1000))

HXobj.py
@@ -12,7 +12,7 @@ class HeatExchanger:
         self.tube_ID = 6/1000 # in meters
         self.length = 0.35 # in meters
         self.D_shell = 0.064 # in meters
-        self.pipearea = 0.25 * self.tube_ID**2 * np.pi
+        self.pipearea = self.tube_count * 0.25 * self.tube_ID**2 * np.pi
         self.sigma = self.pipearea / (0.25* self.D_shell**2 * np.pi)
 
     def summary(self):

moody.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # COLD SIDE data
-cold_flow = np.array([0.6580, 0.6290, 0.5830, 0.5380, 0.4670, 0.3920, 0.3210, 0.2790, 0.2210, 0]) * 1000
+cold_flow = np.array([0.6580, 0.6290, 0.5830, 0.5380, 0.4670, 0.3920, 0.3210, 0.2790, 0.2210, 0]) / 1000
 cold_pressure = np.array([0.1584, 0.1958, 0.2493, 0.3127, 0.3723, 0.4436, 0.4950, 0.5318, 0.5739, 0.7077]) * 100000
 
 # HOT SIDE data
-hot_flow = np.array([0.4360, 0.3870, 0.3520, 0.3110, 0.2600, 0.2290, 0.1670, 0.1180, 0.0690, 0.0010]) * 1000
+hot_flow = np.array([0.4360, 0.3870, 0.3520, 0.3110, 0.2600, 0.2290, 0.1670, 0.1180, 0.0690, 0.0010]) / 1000
 hot_pressure = np.array([0.0932, 0.1688, 0.2209, 0.2871, 0.3554, 0.4041, 0.4853, 0.5260, 0.5665, 0.6239]) * 100000
 
 # Fit 4th-order polynomials
@@ -30,7 +30,7 @@ x_hot = np.linspace(0, max(hot_pressure), 200)
 
 plt.figure(figsize=(10, 5))
 plt.subplot(1, 2, 1)
-plt.plot(cold_pressure, cold_flow,  'bo', label='Cold data')
+plt.plot(cold_pressure, cold_flow, 'bo', label='Cold data')
 plt.plot(x_cold, cold_poly(x_cold), 'b-', label='Cold fit')
 plt.title('Cold Side Fit')
 plt.xlabel('Flowrate (litres/s)')